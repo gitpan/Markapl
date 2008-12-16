113ace481e97b5956c27308659244d9a35055c80
劉康民 <gugod@gugod.org>
2008-12-16 17:30:08 +0000

    Import of GUGOD/Markapl-0.02 from CPAN.
    gitpan-cpan-distribution: Markapl
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GUGOD/Markapl-0.02.tar.gz
    gitpan-cpan-author:       GUGOD
    gitpan-cpan-maturity:     released

--- lib/Markapl.pm
+++ lib/Markapl.pm
@@ -1,151 +1,54 @@
 package Markapl;
-use feature 'say';
 use strict;
 use warnings;
 use Devel::Declare ();
+use Sub::Install qw(install_sub);
 
 use 5.008;
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
-my @stack = ();
+use Markapl::TagHandlers;
 
-{
-    package Markapl::TagHandlers;
-    use strict;
-    use warnings;
+use Markapl::Buffer;
+my @buffer_stack;
 
-    our ($Declarator, $Offset);
-    sub skip_declarator {
-        $Offset += Devel::Declare::toke_move_past_token($Offset);
-    }
+sub new_buffer_frame {
+    my $buf = Markapl::Buffer->new(data => '');
+    unshift @buffer_stack, $buf;
+}
 
-    sub skipspace {
-        $Offset += Devel::Declare::toke_skipspace($Offset);
-    }
+sub end_buffer_frame {
+    shift @buffer_stack;
+}
 
-    sub strip_name {
-        skipspace;
-        if (my $len = Devel::Declare::toke_scan_word($Offset, 1)) {
-            my $linestr = Devel::Declare::get_linestr();
-            my $name = substr($linestr, $Offset, $len);
-            substr($linestr, $Offset, $len) = '';
-            Devel::Declare::set_linestr($linestr);
-            return $name;
-        }
-        return;
-    }
+sub buffer {
+    $buffer_stack[0];
+}
 
-    sub strip_proto {
-        skipspace;
-
-        my $linestr = Devel::Declare::get_linestr();
-        if (substr($linestr, $Offset, 1) eq '(') {
-            my $length = Devel::Declare::toke_scan_str($Offset);
-            my $proto = Devel::Declare::get_lex_stuff();
-            Devel::Declare::clear_lex_stuff();
-            $linestr = Devel::Declare::get_linestr();
-            substr($linestr, $Offset, $length) = '';
-            Devel::Declare::set_linestr($linestr);
-            return $proto;
-        }
-        return;
-    }
-
-    sub shadow {
-        my $pack = Devel::Declare::get_curstash_name;
-        Devel::Declare::shadow_sub("${pack}::${Declarator}", $_[0]);
-    }
-
-    sub make_proto_unwrap {
-        my ($proto) = @_;
-        # my $inject = 'my ($self) = @_;';
-        # $inject .= "my \$attr = { $proto };" if defined($proto) && length($proto);
-        return "";
-    }
-
-    sub inject_if_block {
-        my $inject = shift;
-        skipspace;
-        my $linestr = Devel::Declare::get_linestr;
-        if (substr($linestr, $Offset, 1) eq '{') {
-            substr($linestr, $Offset+1, 0) = $inject;
-            Devel::Declare::set_linestr($linestr);
-        }
-    }
-
-    sub tag_parser_for {
-        my ($tag) = @_;
-        # print "Make tag paser for $tag\n";
-        return sub {
-            local ($Declarator, $Offset) = @_;
-            skip_declarator;
-            my $name = strip_name;
-            my $proto = strip_proto;
-            inject_if_block(
-                make_proto_unwrap($proto)
-            );
-
-            if (defined $name) {
-                $name = join('::', Devel::Declare::get_curstash_name(), $name)
-                    unless ($name =~ /::/);
-                shadow(sub (&) { no strict 'refs'; *{$name} = shift; });
-            } else {
-                shadow(
-                    sub (&) {
-                        my $block = shift;
-                        my $attr = "";
-                        if (defined $proto) {
-                            my @attr;
-                            eval "\@attr = ( $proto )";
-
-                            if (@attr == 1) {
-                                # Special case.
-                                # h1("#myid") { ... }
-
-                                my $css = $attr[0];
-                                while($css =~ /([#\.])(\w+)/g) {
-                                    if ($1 eq '#') {
-                                        $attr .= qq{ id="$2"};
-                                    }
-                                    else {
-                                        $attr .= qq{ class="$2"};
-                                    }
-                                }
-                            }
-                            else {
-                                my ($k, $v) = (shift @attr, shift @attr);
-                                while ($k && $v) {
-                                    $attr .= " $k=\"$v\"";
-                                    ($k, $v) = (shift @attr, shift @attr);
-                                }
-                            }
-                        }
-                        my @s = @stack;
-                        @stack = ();
-                        push @stack, $block->();
-                        push @s, "<${tag}${attr}>", @stack,  "</$tag>";
-                        @stack = @s;
-                        return;
-                    }
-                );
-            }
-        }
-    }
+sub template {
+    my ($name, $code) = @_;
+    my $caller = caller;
+    install_sub({
+        code => $code,
+        into => $caller,
+        as => $name
+    });
 }
 
 sub outs($) {
     my $str = shift;
-    push @stack, $str;
+    Markapl->buffer->append( $str );
     return "";
 }
 
 sub render {
     my ($self, $template) = @_;
-    @stack = ();
+
+    Markapl->new_buffer_frame;
     if (my $sub = $self->can($template)) {
-        push @stack, $sub->($self);
+        $sub->($self);
     }
-    return join("", @stack);
+    return Markapl->end_buffer_frame->data;
 }
 
 sub _get_tag_list {
@@ -174,6 +77,7 @@ sub import {
         no strict;
         *{"${caller}::render"} = \&render;
         *{"${caller}::outs"} = \&outs;
+        *{"${caller}::template"} = \&template;
     }
 
     my $config = {};
@@ -206,55 +110,129 @@ This document describes Markapl version 0.01
     package MyView;
     use Markapl;
 
-    sub a_good_page {
+    tempalte '/a/page.html' => sub {
         h1("#title") { "Hi" };
         p(".first") { "In the begining, lorem ipsum...." };
         p(style => "color: red;") { "But...." };
     }
 
     package main;
-    MyView->show("a_good_page");
+    my $output = MyView->render("/a/page.html");
+
+=head1 TUTORIAL
+
+Here's a short guide how to use this module. You can skip this
+tutorial section if you're already using L<Template::Declare>, since
+it's exactly the same.
+
+First of all, you need a sole package for defining your tempaltes,
+let's call it "MyView" in the example.  Then you C<use Markapl> (but
+not use base), then your view package will be installed many
+subroutines automatically:
+
+    package MyView;
+    use Markapl;
+
+To define a template, use C<template> function like this:
+
+    tempalte '/page.html' => sub {
+        h1("#title") { "Hi" };
+        p(".first") { "In the begining, lorem ipsum...." };
+        p(style => "color: red;") { "But...." };
+    }
+
+To render it, call C<render> function:
+
+    my $out = MyView->render("/page.html");
+
+Besides these two functions, C<Markapl> also exports about another 120
+tag functions that are named after HTML tags, for exapmle, h1, h2, h3,
+div, p, and span. Almost all HTML tags are defined as a function.
+
+In your template, if you say:
+
+    h1 { "Hi" };
+
+It'll be rendered as:
+
+    <h1>Hi</h1>
+
+The block after C<h1> is an anonymous sub-routine, and the return
+value of which will become the content of the C<h1> tag.
+
+If you want to add attributes to the tag, do it like this:
+
+    div(id => "example", class => "lipsum") { "Lorem ipsum" };
+
+Alternatively, you can use CSS selector syntax to quickly defined
+id and class attribute:
+
+    div("#example") { "Lorem ipsum" };
+
+That only works when the attribute list contain excatly one string inside.
+
+A special function C<outs> need to be used to concatinate strings with
+inline elements:
+
+    p {
+        outs "Hello, ";
+        a(href => "/users/gugod") { "gugod" };
+    }
 
 =head1 DESCRIPTION
 
+This is a new try to use L<Devel::Declare> to change the Perl5
+language. It learns pretty much everything from L<Tempalte::Declare>,
+and has similar interface. With only one difference: how element
+attributes are defined.
+
+In L<Template::Declare>, it goes like:
+
+    h1 {
+        id is "title";
+        outs "Hi";
+    };
+
+In here, it is:
+
+    h1(id => "title") { "Hi" };
+
+Or a shorthand for "id" attribute:
+
+    h1("#title") { "Hi" };
 
 =head1 INTERFACE 
 
-
 =over
 
-=item new()
+=item template($name, $code);
+
+Defines a template.
+
+=item render($name)
+
+You need to call it as class method like,
+
+   MyView->render("/foo.html");
+
+Or
+
+   render MyView, "/foo.html";
+
+If you happen to like this style.
+
+Doesn't support tempalte variable yet. Stay tuned.
+
+=item outs($str);
+
+Should be used in side a template body. It appends C<$str> to current
+output buffer frame.
 
 =back
-
-=head1 DIAGNOSTICS
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-Markapl requires no configuration files or environment variables.
 
 =head1 DEPENDENCIES
 
-None.
-
-=head1 INCOMPATIBILITIES
-
-None reported.
+C<Devel::Declare>
 
 =head1 BUGS AND LIMITATIONS
 
@@ -263,7 +241,6 @@ No bugs have been reported.
 Please report any bugs or feature requests to
 C<bug-markapl@rt.cpan.org>, or through the web interface at
 L<http://rt.cpan.org>.
-
 
 =head1 AUTHOR
 
